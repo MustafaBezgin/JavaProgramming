833e7474da3565a7cd281a5a0f0f37bc40d9f6a8
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-09-26 23:10:01 +0300

    Day57 - Lambda Expressions task1 is completed

---
+++ src/day57_LambdaExpressions_Continue/Orange.java
@@ -0,0 +1,16 @@
+package day57_LambdaExpressions_Continue;
+
+import day56_Lombok_LambdaExpressions.Color;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Orange {
+    private int weight;
+    private Color color;
+}
---
+++ src/day57_LambdaExpressions_Continue/OrangeFormatter.java
@@ -0,0 +1,7 @@
+package day57_LambdaExpressions_Continue;
+
+
+@FunctionalInterface
+public interface OrangeFormatter {
+    String accept(Orange orange);
+}
