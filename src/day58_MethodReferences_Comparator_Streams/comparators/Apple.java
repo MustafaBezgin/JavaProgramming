d53ec4a0fed855521028b6e0dd50fc6963073d4a
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-09-27 18:49:29 +0300

    Deleted classes added again

---
+++ src/day58_MethodReferences_Comparator_Streams/comparators/Apple.java
@@ -0,0 +1,17 @@
+package day58_MethodReferences_Comparator_Streams.comparators;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Apple {
+
+    private double weight;
+    private Color color;
+
+}
---
+++ src/day58_MethodReferences_Comparator_Streams/double_colon_operators/Calculate.java
@@ -0,0 +1,8 @@
+package day58_MethodReferences_Comparator_Streams.double_colon_operators;
+
+@FunctionalInterface
+public interface Calculate {
+
+    void calculate (int x, int y);
+
+}
