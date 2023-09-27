ad89e8a058a39c419ae744adf49ec108ddb58b22
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-09-27 17:35:35 +0300

    Comparator (interfaces) contınue

--- src/day58_MethodReferences_Comparator_Streams/comparators/AppleTest.java
+++ src/day58_MethodReferences_Comparator_Streams/comparators/AppleTest.java
@@ -1,7 +1,10 @@
 package day58_MethodReferences_Comparator_Streams.comparators;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class AppleTest {
 
@@ -12,8 +15,14 @@ public class AppleTest {
         inventory.add(new Apple(100, Color.RED));
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.YELLOW));
-        
-        
+
+        Comparator<Apple> sortApple = comparing((Apple apple) -> apple.getWeight());
+        inventory.sort(sortApple);
+        System.out.println(inventory);
+
+
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
         
     }
 
