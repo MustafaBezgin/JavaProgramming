cc46978abfbd6af766f435063c2a47b790bf82f3
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-09-27 16:58:53 +0300

    Comparator (interfaces) contınue

---
+++ src/day58_MethodReferences_Comparator_Streams/comparators/AppleTest.java
@@ -0,0 +1,20 @@
+package day58_MethodReferences_Comparator_Streams.comparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList<>();
+        inventory.add(new Apple(300, Color.GREEN));
+        inventory.add(new Apple(100, Color.RED));
+        inventory.add(new Apple(200, Color.GREEN));
+        inventory.add(new Apple(50, Color.YELLOW));
+        
+        
+        
+    }
+
+}
