1147a68c9dbe9a0f13671e5a6ee602cbf3ea1aca
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-09-27 18:50:48 +0300

    Deleted stream classes added again

---
+++ src/day58_MethodReferences_Comparator_Streams/stream/Course.java
@@ -0,0 +1,13 @@
+package day58_MethodReferences_Comparator_Streams.stream;
+
+public class Course {
+
+    private String name;
+    private int courseId;
+
+
+    public Course(String name, int courseId) {
+        this.name = name;
+        this.courseId = courseId;
+    }
+}
