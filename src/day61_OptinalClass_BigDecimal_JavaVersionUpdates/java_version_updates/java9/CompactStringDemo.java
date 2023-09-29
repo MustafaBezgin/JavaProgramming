f0773b087bb376f7faa8ad9cd4338612db70c595
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-09-29 14:04:22 +0300

    Factory Methods - Compact String

---
+++ src/day61_OptinalClass_BigDecimal_JavaVersionUpdates/java_version_updates/java9/CompactStringDemo.java
@@ -0,0 +1,10 @@
+package day61_OptinalClass_BigDecimal_JavaVersionUpdates.java_version_updates.java9;
+
+public class CompactStringDemo {
+
+    public static void main(String[] args) {
+
+
+        String abc = "hello";
+    }
+}
